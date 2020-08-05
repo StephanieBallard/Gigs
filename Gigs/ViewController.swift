913430f625ed992e8fb6d04e348c0d4418fa13f2
Stephanie <user.name>
2020-08-05 12:32:53 -0700

    initial commit

---
+++ Gigs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Stephanie Ballard on 8/5/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
